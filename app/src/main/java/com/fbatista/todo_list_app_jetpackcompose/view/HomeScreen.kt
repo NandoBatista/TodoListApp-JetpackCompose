ac1209053243dedd85d5d5ee7c611379f7b015cf
fbatista <fernando.batista@mc1.com.br>
2023-04-28 13:43:57 -0300

    implemented grade list filtering

--- app/src/main/java/com/fbatista/todo_list_app_jetpackcompose/view/HomeScreen.kt
+++ app/src/main/java/com/fbatista/todo_list_app_jetpackcompose/view/HomeScreen.kt
@@ -3,9 +3,11 @@ package com.fbatista.todo_list_app_jetpackcompose.view
 import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
@@ -22,51 +24,67 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.fbatista.todo_list_app_jetpackcompose.model.Note
 import com.fbatista.todo_list_app_jetpackcompose.ui.ImageBackground
-import com.fbatista.todo_list_app_jetpackcompose.ui.theme.ColorBackgroundNote
+import com.fbatista.todo_list_app_jetpackcompose.ui.theme.ColorSearchBar
+import java.util.Date
 
 @Composable
 @Preview
 fun HomeScreen() {
+    val listNotes = listOf(Note("Palmeiras","SPFC, consectetur, adipisci velit...", Date()), Note("Lorem ipsum","Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...", Date()))
     ImageBackground()
     Column(
         modifier = Modifier
             .padding(16.dp)
     ) {
-        SearchBar()
-        NoteItemScreen()
-        NoteItemScreen()
-        NoteItemScreen()
-        NoteItemScreen()
-        NoteItemScreen()
+        SearchNotes(listNotes)
     }
 }
 
 @Composable
-fun SearchBar() {
+fun SearchNotes(notes: List<Note>) {
     val (value, onValueChange) = remember { mutableStateOf("") }
     val maxLength = 30
+    val filteredNotes = if (value.isEmpty()) {
+        notes
+    } else {
+        notes.filter {
+            it.title.contains(value, ignoreCase = true) ||
+                    it.description.contains(value, ignoreCase = true) ||
+                    it.createdDate.toString().contains(value, ignoreCase = true)
+        }
+    }
 
-    TextField(
-        value = value,
-        onValueChange = { newValue ->
-            if (newValue.length <= maxLength) {
-                onValueChange(newValue)
-                Log.d("SearchBar", "onValueChange: $newValue")
-            }
-        },
-        textStyle = TextStyle(fontSize = 18.sp),
-        leadingIcon = { Icon(Icons.Filled.Search, null, tint = Color.Gray) },
-        modifier = Modifier
-            .padding(start = 4.dp, top = 16.dp, end = 4.dp, bottom = 44.dp)
-            .background(ColorBackgroundNote, RoundedCornerShape(12.dp))
-            .fillMaxWidth(),
-        placeholder = { Text(text = "Procurar anotação") },
-        colors = TextFieldDefaults.textFieldColors(
-            focusedIndicatorColor = Color.Transparent,
-            unfocusedIndicatorColor = Color.Transparent,
-            backgroundColor = Color.Transparent,
-            cursorColor = Color.DarkGray
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        TextField(
+            value = value,
+            onValueChange = { newValue ->
+                if (newValue.length <= maxLength) {
+                    onValueChange(newValue)
+                    Log.d("SearchBar", "onValueChange: $newValue")
+                }
+            },
+            textStyle = TextStyle(fontSize = 18.sp),
+            leadingIcon = { Icon(Icons.Filled.Search, null, tint = Color.Gray) },
+            modifier = Modifier
+                .padding(start = 4.dp, top = 16.dp, end = 4.dp, bottom = 22.dp)
+                .background(ColorSearchBar, RoundedCornerShape(12.dp))
+                .fillMaxWidth(),
+            placeholder = { Text(text = "Procurar anotação") },
+            colors = TextFieldDefaults.textFieldColors(
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                backgroundColor = Color.Transparent,
+                cursorColor = Color.DarkGray
+            )
         )
-    )
+        LazyColumn {
+            items(filteredNotes) { note ->
+                NoteItemScreen(note)
+            }
+        }
+    }
 }
--- app/src/main/java/com/fbatista/todo_list_app_jetpackcompose/view/NoteItemScreen.kt
+++ app/src/main/java/com/fbatista/todo_list_app_jetpackcompose/view/NoteItemScreen.kt
@@ -1,11 +1,9 @@
 package com.fbatista.todo_list_app_jetpackcompose.view
 
-
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import com.fbatista.todo_list_app_jetpackcompose.model.Note
@@ -13,23 +11,19 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.fbatista.todo_list_app_jetpackcompose.ui.theme.ColorNote
 import java.text.SimpleDateFormat
-import java.util.Date
 import java.util.Locale
 
 @Composable
-@Preview
-fun NoteItemScreen() {
-    var note = Note("Lorem ipsum","Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...", Date())
+fun NoteItemScreen(note: Note) {
     val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
     val createdDateStr = dateFormat.format(note.createdDate)
 
     Surface(
         modifier = Modifier
-            .padding(start = 4.dp, top = 16.dp, end = 4.dp)
+            .padding(start = 4.dp, bottom = 12.dp, end = 4.dp)
             .fillMaxWidth(),
         shape = RoundedCornerShape(12.dp),
         elevation = 8.dp,
